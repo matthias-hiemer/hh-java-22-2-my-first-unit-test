bbe50da89f6e39a5f477fbff02ca953650670cd1
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-08-31 14:26:00 +0200

    Add Multiplier initially

---
+++ src/main/java/Multiplier.java
@@ -0,0 +1,16 @@
+public class Multiplier {
+
+    public static void main(String[] args) {
+        int firstNumber = 2;
+        int secondNumber = 21;
+
+        int result =  multiply(firstNumber, secondNumber);
+
+        System.out.println("Das Ergebnis ist: " + result);
+    }
+
+    public static int multiply(int value1, int value2) {
+        return value1 * value2;
+    }
+
+}
